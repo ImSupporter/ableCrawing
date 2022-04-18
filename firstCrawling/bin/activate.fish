5628bfd52031fc702af4a379dfabd338bf31eaf0
parktaehyun <adam990901@gmail.com>
2022-04-18 14:51:40 +0900

    첫번째 마이그레이션

--- firstCrawling/bin/activate.fish
+++ firstCrawling/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/parktaehyun/SmileHunter/Crawling/firstCrawling"
+set -gx VIRTUAL_ENV "/Users/parktaehyun/smilhunter/ableCrawing/firstCrawling"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- firstCrawling/src/ItemManager.py
+++ firstCrawling/src/ItemManager.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 import pymysql
 from dataclasses import dataclass
+
+
+host_address = '13.125.254.122'
 @dataclass
 class Item:
     brand: str = None
@@ -12,7 +15,7 @@ class Item:
     
 
 def insert(brand, item_name, item_code, main_category, sub_category, price, redirect_url):
-    db_con = pymysql.connect(host='localhost',port=3306, user='abletest',passwd='xogus',charset='utf8',db='testdb')
+    db_con = pymysql.connect(host=host_address,port=3306, user='abletest',passwd='xogus',charset='utf8',db='testdb')
     db_cursor = db_con.cursor()
     insert_sql = 'INSERT INTO item (brand, item_name, item_code, main_category, sub_category, price, redirect_url) VALUES (%s, %s, %s, %s, %s, %s, %s)'
     db_cursor.execute(insert_sql,(brand, item_name, item_code, main_category, sub_category, price, redirect_url))
@@ -20,7 +23,7 @@ def insert(brand, item_name, item_code, main_category, sub_category, price, redi
     db_con.close() 
 
 def search(item_code):
-    db_con = pymysql.connect(host='localhost',port=3306, user='abletest',passwd='xogus',charset='utf8',db='testdb')
+    db_con = pymysql.connect(host=host_address,port=3306, user='abletest',passwd='xogus',charset='utf8',db='testdb')
     db_cursor = db_con.cursor()
     search_sql = 'SELECT * FROM item where item_code = %s'
     db_cursor.execute(search_sql,(item_code))
@@ -34,4 +37,4 @@ def search(item_code):
 
 
 if __name__  == '__main__':
-    print(search('111'))
+    print(search('HC9962'))
