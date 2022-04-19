ba79668b8f5e94bbed942d2dbff3abb9203e4449
parktaehyun <adam990901@gmail.com>
2022-04-19 21:36:20 +0900

    헬스장 데이터 찾기

--- firstCrawling/src/gym.py
+++ firstCrawling/src/gym.py
@@ -1,3 +1,5 @@
+import json
+import requests
 import ItemManager
 from time import sleep
 from selenium import webdriver   
@@ -15,9 +17,38 @@ def set_chrome_driver():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), 
                               options=chrome_options)
     return driver
-#----------------------------------------------------------------------------------------------------------------
-driver = set_chrome_driver()
-driver.get('https://map.kakao.com/')
 
-sleep(2)
-driver.close()
+def send_api(y:float , x:float, page:int = 1, query:str = '헬스장'):
+    API_URL = 'https://dapi.kakao.com/v2/local/search/keyword'
+    headers = {'Authorization':'KakaoAK bbea4260b365b4a526a908b0d42b3b3d'}
+    body = {
+        'y': y,
+        'x' : x,
+        'radius' : '20000',
+        'page' : page,
+        'size' : 15,
+        'query' : query,
+        'sort' : 'distance'
+    }
+
+    response = requests.get(API_URL, data=body, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(response.content)
+#----------------------------------------------------------------------------------------------------------------
+num = 1
+page = 1
+for i in range(99999999):
+    response = send_api(y = 37.514322572335935, x = 127.06283102249932,page=page)
+    meta = response['meta']
+    if page == 1:
+        print('전체 결과 수',':',meta['total_count'])
+    document = response['documents']
+    for p in document:
+        print(num, ':', p['place_name'],':', p['road_address_name'])
+        num+=1
+    print("---------------------------------------------------")
+    page+=1
+    if meta['is_end'] is True:
+        break
