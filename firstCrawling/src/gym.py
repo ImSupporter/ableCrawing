ecdb4e28e280d824307b1926a3ba4f59b3aae55d
ImSupporter <adam990901@gmail.com>
2022-04-20 21:08:30 +0900

    장소 검색 개발중(검색완료) -> DB에 넣기만 해

--- firstCrawling/src/gym.py
+++ firstCrawling/src/gym.py
@@ -9,11 +9,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+import pandas as pd
+from pyproj import Proj, transform
 
 #----------------------------------------------------------------------------------------------------------------
-def set_chrome_driver():
+def set_chrome_driver_mobile():
+    mobile_emulation = {'deviceName':'iPhone X'}
     chrome_options = webdriver.ChromeOptions()
     #chrome_options.add_argument('headless')
+    #chrome_options.add_experimental_option('mobileEmulation', mobile_emulation)
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), 
                               options=chrome_options)
     return driver
@@ -37,18 +41,54 @@ def send_api(y:float , x:float, page:int = 1, query:str = '헬스장'):
     else:
         print(response.content)
 #----------------------------------------------------------------------------------------------------------------
-num = 1
-page = 1
-for i in range(99999999):
-    response = send_api(y = 37.514322572335935, x = 127.06283102249932,page=page)
-    meta = response['meta']
-    if page == 1:
-        print('전체 결과 수',':',meta['total_count'])
-    document = response['documents']
-    for p in document:
-        print(num, ':', p['place_name'],':', p['road_address_name'])
-        num+=1
-    print("---------------------------------------------------")
-    page+=1
-    if meta['is_end'] is True:
-        break
+#세팅
+FILE_PATH = '/Users/parktaehyun/SmileHunter/Crawling/firstCrawling/fulldata_gym.csv'
+    # Projection 정의
+    # 중부원점(Bessel): 서울 등 중부지역 EPSG:2097
+proj_1 = Proj(init='epsg:2097')
+    # WGS84 경위도: GPS가 사용하는 좌표계 EPSG:4326
+proj_2 = Proj(init='epsg:4326')
+
+    
+driver = set_chrome_driver_mobile()
+
+# csv 파일 불러오기
+total_gym = pd.read_csv(FILE_PATH,low_memory=False)
+
+# csv 파일 전체 조회
+on_gym=[]
+print('전체 데이터 수 :',len(total_gym))
+for i in range(len(total_gym)):
+    gym = total_gym.loc[i]
+    status_code = gym['영업상태구분코드']
+    
+    # 영업중인 장소(status code == 1)만 검색 후보에 추가
+    if(status_code == 1):
+        gym_name = gym['사업장명']
+        road_address = gym['도로명전체주소']
+        address = gym['소재지전체주소']
+        on_gym.append([gym_name,road_address, address])
+        
+#검색(크롤링 시작)
+for i in range(100):
+    print(i,on_gym[i])
+    driver.get('https://m.map.naver.com/#/search')
+    sleep(0.5)
+    #검색어 입력
+    search_box = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,'//*[@id="ct"]/div[1]/div[1]/form/div/div[2]/div/span[1]/input'))
+    )
+    if type(on_gym[i][1]) is not float: #도로명로 검색
+        search_box.send_keys(on_gym[i][0]+" "+on_gym[i][1].split(',')[0])
+    else:   #도로명 주소가 없을 때 전체 주소로 검색
+        search_box.send_keys(on_gym[i][0]+" "+on_gym[i][2].split(',')[0])
+    driver.find_element(By.XPATH,'//*[@id="ct"]/div[1]/div[1]/form/div/div[2]/div/span[2]/button[2]').click()
+    sleep(0.5)
+    
+    try:
+        rank1_result = driver.find_element(By.XPATH,'//*[@id="ct"]/div[2]/ul/li/div[1]/a/div/strong').text
+    except:
+        print('결과 없음')
+    sleep(1)
+
+
